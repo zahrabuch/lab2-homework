95857608540abe76ac00a476d219896064541947
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2025-10-17 14:51:39 -0500

    Task 3

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -26,6 +26,7 @@ public class Main {
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
         // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
         // return statement included so that the starter code can compile and run.
-        return -1;
+        List<String> breedsList = breedFetcher.getSubBreeds(breed);
+        return breedsList.size();
     }
 }
